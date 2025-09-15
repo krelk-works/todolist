69b9ea57b891e02f0539eeef0f103b81a7b25b38
krelk-works <leo.craciun97@gmail.com>
2025-09-16 00:47:41 +0200

    added translations for chinese, japanese, russian and ukrainian

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,8 @@
-import { useEffect } from 'react';
-import { useTranslation } from 'react-i18next';
 import { Footer } from '@/components/Footer';
 import { Header } from '@/components/Header';
 import { Tasks } from '@/components/Tasks';
 
 export const App = () => {
-    const { i18n } = useTranslation();
-
-    const changeLanguageHandler = (lang: string) => {
-        i18n.changeLanguage(lang);
-    };
-
-    useEffect(() => {
-        changeLanguageHandler('tr');
-    }, []);
-
     return (
         <div className='min-h-screen min-w-screen flex flex-col'>
             {/* 100vh y columna */}
